98c792950967152daa9270823d1981a33d504c33
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-09-27 16:30:35 +0530

    added normal as well as validation for conflicts

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -4,5 +4,55 @@ class Interview < ApplicationRecord
 	has_and_belongs_to_many :users
 
 	accepts_nested_attributes_for :interviews_users
+    
+    validates :interview_date, presence: true
+    validates :start_time, presence: true
+    validates :end_time, presence: true
+
+    validate :end_before_start
+    validate :conflicts
+
+    private
+    	
+    	def end_before_start
+    		if end_time<=start_time
+    			errors.add(:end_time, "must be after start time")
+    		end
+    	end
+
+    	def conflicts
+    		
+    		cur_start = start_time.to_i + interview_date.to_time.to_i
+    		cur_end = end_time.to_i + interview_date.to_time.to_i
+             
+            user1check = User.where(:email => users.first.email).first
+            user2check = User.where(:email => users.last.email).first
+
+            if user1check
+                schedules = User.where(:id => user1check.id).first.interviews
+                schedules.each do |schedule|
+                    schedule_start = schedule.start_time.to_i + schedule.interview_date.to_time.to_i
+                    schedule_end = schedule.end_time.to_i + schedule.interview_date.to_time.to_i
+
+                    if (cur_start >= schedule_start && cur_start <= schedule_end) || ((cur_end >= schedule_start && cur_end <= schedule_end))
+                        errors.add(:start_time, "conflicts ")
+                    end
+                end    
+            end
+
+            if user2check
+                schedules = User.where(:id => user2check.id).first.interviews
+                schedules.each do |schedule|
+                    schedule_start = schedule.start_time.to_i + schedule.interview_date.to_time.to_i
+                    schedule_end = schedule.end_time.to_i + schedule.interview_date.to_time.to_i
+
+                    if (cur_start >= schedule_start && cur_start <= schedule_end) || ((cur_end >= schedule_start && cur_end <= schedule_end))
+                        errors.add(:start_time, "conflicts ")
+                    end
+                end    
+            end
+        end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
 	
 	has_many :interviews_users
 	has_and_belongs_to_many :interviews
-	
+
+	validates :name, presence: true
+	validates :email, presence: true
+
 end
